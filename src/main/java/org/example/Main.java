2b453a005ba9781ece949778c68201b9306e7dd7
ohseonghyeon <wert3976@gmail.com>
2025-03-04 16:44:28 +0900

    문제 풀이

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,48 +1,34 @@
 package org.example;
 
-import java.io.*;
-import java.util.concurrent.atomic.AtomicInteger;
-
 public class Main {
 
-    static AtomicInteger age = new AtomicInteger();
-    static int askCnt = 0;
+    public static void main(String[] args) throws Exception {
+        System.out.println(ggdResult());
+    }
 
-    public static void main(String[] args) {
-        try(
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))
-        ) {
-            bw.write("당신의 나이를 입력해주세요.\n");
-            bw.flush();
-            askYourAge(br, bw);
-            if(askCnt >= 3) {
-                bw.write(String.format("올바른 나이를 %d회 입력하지 않아 프로그램을 종료함.", askCnt));
-                bw.flush();
-                return;
+    public static String ggdResult() {
+        StringBuilder sb = new StringBuilder();
+        appendGooGooDan(sb, 2, 9, 1, 9, 1);
+        return sb.toString();
+    }
+
+    public static void appendGooGooDan(StringBuilder sb, int sd, int ed, int sm, int em, int danPrintAxis) {
+        if(sd > ed) {
+            return;
+        }
+        
+        if(sm <= em) {
+            if(sd <= em && sm == danPrintAxis) {
+                sb.append(String.format("%d 단 출력\n", sd));
             }
-            int iAge = age.get();
-            if      (iAge >=  0 && iAge < 10) bw.write("유아/아동\n");
-            else if (iAge >= 10 && iAge < 20) bw.write("10대\n");
-            else if (iAge >= 20 && iAge < 30) bw.write("20대\n");
-            else if (iAge >= 30 && iAge < 40) bw.write("30대\n");
-            else if (iAge >= 40)              bw.write("40대 이상\n");
-            bw.flush();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+
+            sb.append(String.format("%d * %d = %d\n", sd, sm, sd*sm));
+            appendGooGooDan(sb, sd, ed, ++sm, em, danPrintAxis);
+
+            if(sm == 9) {
+                appendGooGooDan(sb, ++sd, ed, danPrintAxis, em, danPrintAxis);
+            }
         }
     }
 
-    private static void askYourAge(BufferedReader br, BufferedWriter bw) throws IOException {
-        try {
-            int inputAge = Integer.parseInt(br.readLine());
-            age.set(inputAge);
-            if(askCnt > 0) askCnt = 0;
-        } catch(NumberFormatException nfe) {
-            if(++askCnt >= 3) return;
-            bw.write("올바른 나이를 입력하시오.\n");
-            bw.flush();
-            askYourAge(br, bw);
-        }
-    }
 }
---
+++ src/main/java/org/example/Solution.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
