61e4017738231143c117d24ed14c5b4e0fb04552
ohseonghyeon <wert3976@gmail.com>
2025-03-04 14:17:20 +0900

    문제 풀이

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,61 +16,61 @@ class Main {
 
         // `==` => 같다.
         if ( a == 10 ) {
-            System.out.println("t");
+            System.out.println("t1");
         }
 
         // `!=` => 같지 않다.
         if ( a != 10 ) {
-            System.out.println("f");
+            System.out.println("f1");
         }
 
         if ( a > 10 ) {
-            System.out.println("f");
+            System.out.println("f2");
         }
 
         if ( a >= 10 ) {
-            System.out.println("t");
+            System.out.println("t2");
         }
 
         int b = 10;
 
         if ( a == b ) {
-            System.out.println("t");
+            System.out.println("t3");
         }
 
         // boolean c => c 에는 오직 true/false 만 담을 수 있다.
         boolean c = a != b;
 
         if ( c ) {
-            System.out.println("f");
+            System.out.println("f3");
         }
 
         if ( c == false ) {
-            System.out.println("t");
+            System.out.println("t4");
         }
 
         // `!` => 반전
         if ( !c ) {
-            System.out.println("t");
+            System.out.println("t5");
         }
 
         // `!` => 반전
         if ( !(!c) ) {
-            System.out.println("f");
+            System.out.println("f4");
         }
 
         boolean d = true;
 
         if ( c != d ) {
-            System.out.println("t");
+            System.out.println("t6");
         }
 
         if ( 20 > 2 && 10 > 3 && true != false && 10 != 10 ) {
-            System.out.println("f");
+            System.out.println("f5");
         }
 
         if ( 10 != 10 || 10 < 2 ) {
-            System.out.println("f");
+            System.out.println("f6");
         }
     }
 }
