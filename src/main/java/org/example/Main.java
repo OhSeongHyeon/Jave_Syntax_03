24da97e7b4d1d8b6f756a8caf2a3fa490789a422
OhSeongHyeon <wert3976@gmail.com>
2025-03-06 11:36:07 +0900

    세팅초기화

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,33 +2,8 @@ package org.example;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-        System.out.println(ggdResult());
-    }
+    public static void main(String[] args) {
 
-    public static String ggdResult() {
-        StringBuilder sb = new StringBuilder();
-        appendGooGooDan(sb, 2, 9, 1, 9, 1);
-        return sb.toString();
-    }
-
-    public static void appendGooGooDan(StringBuilder sb, int sd, int ed, int sm, int em, int danPrintAxis) {
-        if(sd > ed) {
-            return;
-        }
-        
-        if(sm <= em) {
-            if(sd <= em && sm == danPrintAxis) {
-                sb.append(String.format("%d 단 출력\n", sd));
-            }
-
-            sb.append(String.format("%d * %d = %d\n", sd, sm, sd*sm));
-            appendGooGooDan(sb, sd, ed, ++sm, em, danPrintAxis);
-
-            if(sm == 9) {
-                appendGooGooDan(sb, ++sd, ed, danPrintAxis, em, danPrintAxis);
-            }
-        }
     }
 
 }
